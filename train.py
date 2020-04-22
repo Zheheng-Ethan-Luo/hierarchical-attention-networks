dea64e3c90867a24a1804907379c85ddd53314e4
Angus <zhehengluo@gmail.com>
2020-04-22 19:23:45 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,45 +5,35 @@ import torch.nn.functional as F
 from model import HAN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-def train(input,target,han,han_optmz,criterion):
-	han_optmz.zero_grad()
-	
-	claim = input[0]
-	sentences = input[1]
-	
-	loss = 0
-	
-	output = han(claim,sentences)
-	output = torch.log(output)
-	loss += criterion(output,target)
-	
-	loss.backward()
-	
-	han_optmz.step()
-	
-	return loss.item()
-	
-def trainIters(han, instances, n_epoches, print_every=100, learning_rate=0.005):
-	start = time.time()
-	print_loss_total = 0
-	
-	han_optmz = optim.Adagrad(han.parameters(), lr=learning_rate)
-	criterion = nn.BCELoss()
-	for epoch in range(n_epoches):
-		for i in range(len(instances)):
-			input = instances[i][0]
-			target = instances[i][1]
-			
-			loss = train(input,target,han,han_optmz,criterion)
-			print_loss_total += loss
-		
-		if i % print_every ==0:
-			print_loss_avg = print_loss_total/print_every
-			print_loss_total = 0
-			now = time.time()
-			print('%s (%d %d%%) %.4f' % (int(now-start),i,i/(len(instances)*n_epoches),print_loss_avg)
 
+def train_batch(model,instances,n_epoches,batch_size=100,learning_rate=0.01):
+	optmz = optim.Adagrad(model.parameters(), lr=learning_rate)
+	criterion = nn.BCELoss()
+	optmz.zero_grad()
+	n_samples = len(instances)
+	print_every = int(n_samples/20)
+	print_average_loss = 0
+	for epoch in range(n_epoches):
+		print('epoch %s start' % epoch)
+		for i in range(n_samples):
+			claim = instances[i][0]
+			sentences = instances[i][1]
+			target = instances[i][2]
+			output = model(claim,sentences)
+			loss = criterion(output,target)
+			print_average_loss += loss
+			loss = loss/batch_size
+			loss.backward()
+			
+			if (i+1)% batch_size == 0:
+				optmz.step()
+				optmz.zero_grad()
+			if (i+1)% print_every == 0:
+			  print_average_loss /= print_every
+			  print('%s%% finished, loss: %.4f' % (int((i+1)/n_samples*100),print_average_loss))
+			  print_average_loss = 0
+			
 hidden_size = 100
 embedding_size = 50
 han = HAN(embedding_size,hidden_size,1).to(device)
-trainIters(han,instances,20)
+train_batch(han,instances,n_epoches=100)
