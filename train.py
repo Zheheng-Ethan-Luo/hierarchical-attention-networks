3287c16252ef67690aa03ddbd57b23d7b06be4b5
Angus <zhehengluo@gmail.com>
2020-04-22 19:41:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,19 +2,18 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-from model import HAN
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import random
 
 def train_batch(model,instances,n_epoches,batch_size=100,learning_rate=0.01):
 	optmz = optim.Adagrad(model.parameters(), lr=learning_rate)
 	criterion = nn.BCELoss()
-	optmz.zero_grad()
 	n_samples = len(instances)
 	print_every = int(n_samples/20)
 	print_average_loss = 0
 	for epoch in range(n_epoches):
 		print('epoch %s start' % epoch)
+		optmz.zero_grad()
+		random.shuffle(instances)
 		for i in range(n_samples):
 			claim = instances[i][0]
 			sentences = instances[i][1]
@@ -33,7 +32,28 @@ def train_batch(model,instances,n_epoches,batch_size=100,learning_rate=0.01):
 			  print('%s%% finished, loss: %.4f' % (int((i+1)/n_samples*100),print_average_loss))
 			  print_average_loss = 0
 			
-hidden_size = 100
-embedding_size = 50
-han = HAN(embedding_size,hidden_size,1).to(device)
-train_batch(han,instances,n_epoches=100)
+def evaluate(model,instances):
+	tp=fp=tn=fn = 0
+	for instance in instances:
+		claim = instance[0]
+		sentences = instance[1]
+		target = instance[2]
+		output = model(claim,sentences)
+		if output > 0.5:
+			output = 1
+		else:
+			output = 0
+		if target ==1:
+			if output==1:
+				tp+=1
+			else:
+				fn+=1
+		else:
+			if output==1:
+				fp+=1
+			else:
+				tn+=1
+	p = tp/(tp+fp)
+	r = tp/(tp+fn)
+	f1 = 2*p*r/(p+r)
+	return p,r,f1
